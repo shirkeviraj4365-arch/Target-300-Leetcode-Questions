8d40a026f951516633200a94baecfa8bdd99d24f
Viraj_Shirke <shirkeviraj4365@gmail.com>
2026-03-13 13:19:42 +0530

    Time: 84 ms (83.72%), Space: 62 MB (52.96%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -3,28 +3,23 @@ public:
     int maxOperations(vector<int>& nums, int k) {
         int left = 0;
         int right = nums.size() - 1;
-        int i, j = 0;
+        int count = 0;
 
-               int count = 0;
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end()); 
+
         while (left < right) {
-
             int sum = nums[left] + nums[right];
 
-                      if (sum == k) {
+            if (sum == k) {
                 count++;
-                left++;
+                left++;   // Dono pointers ko sirf ek baar move kiya
                 right--;
-            }
-            else if (sum < k) {
+            } else if (sum < k) {
                 left++;
-            }
-            else {
+            } else {
                 right--;
             }
         }
         return count;
     }
 };
-    
-
